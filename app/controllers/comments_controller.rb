11942bf04430269c9f13ac8709c8d5e7d1da2505
rodelio30 <rodeliobdomingojr@gmail.com>
2020-10-27 19:57:56 +0800

    adding all the requirements needed for blog

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,29 @@
+class CommentsController < ApplicationController
+  before_action :set_account
+  before_action :set_comment, except: [:create]
+
+  def create
+    @comment = @account.comments.create(comment_params)
+    redirect_to @account
+  end
+
+  def destroy
+    @comment.destroy
+    redirect_to @account
+  end
+
+private
+
+  def set_account
+    @account = Account.find(params[:account_id])
+  end
+
+  def set_comment
+    @comment = @account.comments.find(params[:id])
+  end
+
+  def comment_params
+    params[:comment].permit(:content)
+  end
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_27_064957) do
+ActiveRecord::Schema.define(version: 2020_10_27_095204) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,14 @@ ActiveRecord::Schema.define(version: 2020_10_27_064957) do
     t.string "position"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.string "content"
+    t.bigint "account_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["account_id"], name: "index_comments_on_account_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -34,4 +42,5 @@ ActiveRecord::Schema.define(version: 2020_10_27_064957) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "comments", "accounts"
 end
