d07451dabd249df0c953ecc440e85d745f4f02e6
rodelio30 <rodeliobdomingojr@gmail.com>
2020-10-28 15:30:47 +0800

    adding table name name

--- db/migrate/20201028072733_create_comments.rb
+++ db/migrate/20201028072733_create_comments.rb
@@ -1,8 +1,8 @@
 class CreateComments < ActiveRecord::Migration[6.0]
   def change
     create_table :comments do |t|
-      t.string :content
       t.string :name
+      t.string :content
       t.references :account, null: false, foreign_key: true
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_27_095204) do
+ActiveRecord::Schema.define(version: 2020_10_28_072733) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2020_10_27_095204) do
   end
 
   create_table "comments", force: :cascade do |t|
+    t.string "name"
     t.string "content"
     t.bigint "account_id", null: false
     t.datetime "created_at", precision: 6, null: false
